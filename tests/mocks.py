63143694ee18256c36ffaec2c631f4c227cef27d
enze-l <leon.enzenberger@protonmail.com>
2022-12-06 19:52:02 +0100

    Wip add test with mocked dependencies

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,7 +1,10 @@
 import sys
-from unittest.mock import Mock
+from unittest.mock import MagicMock
+
+network = MagicMock()
+machine = MagicMock()
 
 
 def init_micropython_mock_modules():
-    sys.modules['network'] = Mock()
-    sys.modules['machine'] = Mock()
+    sys.modules['network'] = network
+    sys.modules['machine'] = machine
--- tests/test_wifi_client.py
+++ tests/test_wifi_client.py
@@ -1,6 +1,7 @@
 import unittest
-from unittest.mock import patch, MagicMock
 from source.networking.wifi_client import WifiClient
+from tests.mocks import network
+from unittest.mock import MagicMock, Mock
 
 
 class TestWifi(unittest.TestCase):
@@ -8,10 +9,25 @@ class TestWifi(unittest.TestCase):
         self.sample_ssid = "sample_ssid"
         self.sample_password = "sample_password"
 
-    @patch("network", MagicMock)
-    def test_wifi_should_connect(self):
-
+    def test_wifi_should_start(self):
         wifi = WifiClient(self.sample_ssid, self.sample_password)
+        wifi.start()
+
+    def test_wifi_should_establish_connection(self):
+        wifi = WifiClient(self.sample_ssid, self.sample_password)
+
+        active = Mock()
+        connect = Mock()
+        isconnected = Mock()
+        network.WLAN().active = active
+        network.WLAN().connect = connect
+        network.WLAN().isconnected = isconnected
+
+        wifi._WifiClient__establish_connection()
+
+        active.assert_called_once()
+        connect.assert_called_once()
+        isconnected.assert_called_once()
 
 
 if __name__ == "__main__":
