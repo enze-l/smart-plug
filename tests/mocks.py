bb002a8f28e1b95cf346983e27dc14f1da6918eb
enze-l <leon.enzenberger@protonmail.com>
2022-12-07 14:56:34 +0100

    Lint scripts

--- tests/mocks.py
+++ tests/mocks.py
@@ -3,5 +3,5 @@ from unittest.mock import Mock
 
 
 def init_micropython_mock_modules():
-    sys.modules['network'] = Mock()
-    sys.modules['machine'] = Mock()
+    sys.modules["network"] = Mock()
+    sys.modules["machine"] = Mock()
