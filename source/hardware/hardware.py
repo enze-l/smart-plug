f34702cde9875ae0951c5f77b1315fa07e8ca552
enze-l <leon.enzenberger@protonmail.com>
2022-12-13 16:18:01 +0100

    - Remove placeholder class for switch in favor of the generalization of buttons - Add configuration for the switch

--- source/hardware/hardware.py
+++ source/hardware/hardware.py
@@ -4,4 +4,5 @@ import config.config as config
 
 led = BinaryOutput(config.LED_PIN, config.LED_IS_INVERTED)
 relay = BinaryOutput(config.RELAY_PIN, config.RELAY_IS_INVERTED)
-button = Button(config.BUTTON_PIN, config.BUTTON_IS_INVERTED)
+button_internal = Button(config.BUTTON_PIN, config.BUTTON_IS_INVERTED)
+button_external = Button()
