c996f5c17c256dd61c0536e298a1fa1e184410a2
enze-l <leon.enzenberger@protonmail.com>
2022-12-13 15:04:00 +0100

    Move functionality from button to debouncer

--- source/hardware/button.py
+++ source/hardware/button.py
@@ -1,32 +1,15 @@
-from machine import Pin
-import time
+from .utilities.debouncer import Debouncer
 
 
 class Button:
     def __init__(self, pin, inverted_input, debounce_delay_ms=50):
-        self.pin = Pin(pin, Pin.IN, Pin.PULL_UP)
         self.inverted_input = inverted_input
         self.function = None
         self.on_value = not inverted_input
         self.off_value = inverted_input
-        self.pin.irq(handler=self.__execute_function_debounced)
 
-        self.debounce_delay = debounce_delay_ms
-        self.last_click_time = time.ticks_ms()
-        self.last_button_state = inverted_input
+        self.debouncer = Debouncer(pin, self.on_value, debounce_delay_ms)
 
     def set_function(self, function):
-        self.function = function
+        self.debouncer.set_function(function)
 
-    def __execute_function_debounced(self, irq):
-        time_of_click = time.ticks_ms()
-        current_button_state = self.pin.value()
-        if current_button_state == self.last_button_state:
-            return
-        min_elapsed_time = time.ticks_add(self.last_click_time, self.debounce_delay)
-        if time.ticks_diff(min_elapsed_time, time_of_click) > 0:
-            return
-        self.last_click_time = time_of_click
-        self.last_button_state = current_button_state
-        if current_button_state:
-            self.function()
--- source/hardware/utilities/debouncer.py
+++ source/hardware/utilities/debouncer.py
@@ -1,9 +1,28 @@
 import time
+from machine import Pin
 
 
 class Debouncer:
-    def __init__(self, debounce_delay_ms=50):
+    def __init__(self, pin, on_state, debounce_delay_ms):
+        self.function = None
+        self.pin = Pin(pin, Pin.IN, Pin.PULL_UP)
         self.debounce_delay = debounce_delay_ms
         self.last_click_time = time.ticks_ms()
-        self.last_button_state = inverted_input
+        self.last_button_state = on_state
+        self.pin.irq(handler=self.__debounce_function)
 
+    def set_function(self, function):
+        self.function = function
+
+    def __debounce_function(self, irq):
+        time_of_click = time.ticks_ms()
+        current_button_state = self.pin.value()
+        if current_button_state == self.last_button_state:
+            return
+        min_elapsed_time = time.ticks_add(self.last_click_time, self.debounce_delay)
+        if time.ticks_diff(min_elapsed_time, time_of_click) > 0:
+            return
+        self.last_click_time = time_of_click
+        self.last_button_state = current_button_state
+        if current_button_state:
+            self.function()
