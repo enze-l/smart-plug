f16b05148cae12d51be4982b9b20e0b6ceabadc8
enze-l <leon.enzenberger@protonmail.com>
2022-12-14 14:46:33 +0100

    Remove Thermometer stub

--- source/hardware/hardware.py
+++ source/hardware/hardware.py
@@ -4,5 +4,6 @@ import config.config as config
 
 led = BinaryOutput(config.LED_PIN, config.LED_IS_INVERTED)
 relay = BinaryOutput(config.RELAY_PIN, config.RELAY_IS_INVERTED)
+
 button_internal = Button(config.BUTTON_INTERNAL_PIN, config.BUTTON_INTERNAL_IS_INVERTED)
 button_external = Button(config.BUTTON_EXTERNAL_PIN, config.BUTTON_EXTERNAL_IS_INVERTED)
--- source/main.py
+++ source/main.py
@@ -1,10 +1,6 @@
 from config.config import WIFI_SSID, WIFI_PASSWORD
 from networking.wifi_client import WifiClient
-import hardware.hardware as hardware
 
 wifi_client = WifiClient(WIFI_SSID, WIFI_PASSWORD)
 wifi_client.start()
 
-hardware.button_internal.set_on_release_function(hardware.led.turn_off)
-hardware.button_internal.set_on_click_function(hardware.led.turn_on)
-hardware.button_internal.set_on_toggle_function(hardware.led.get_on_state)
