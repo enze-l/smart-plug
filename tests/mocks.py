54cd64bbaa3feb182005435ce6b501129d81d3ce
enze-l <leon.enzenberger@protonmail.com>
2022-12-19 20:13:49 +0100

    Make first test for awattar api

--- tests/mocks.py
+++ tests/mocks.py
@@ -6,3 +6,6 @@ def init_micropython_mock_modules():
     sys.modules["network"] = Mock()
     sys.modules["machine"] = Mock()
     sys.modules["time"] = Mock()
+    sys.modules["urequests"] = Mock()
+    sys.modules["uasyncio"] = Mock()
+    sys.modules["micropython"] = Mock()
--- tests/test_awattar_api.py
+++ tests/test_awattar_api.py
@@ -1,11 +1,19 @@
 from unittest import TestCase
+import uasyncio
 from unittest.mock import Mock
 from source.api.awattar.awattar_api import AwattarApi
 
 
 class TestAwattarAPI(TestCase):
-    def test_API_should_start(self):
+    async def test_API_should_start(self):
         hardware = Mock()
         api = AwattarApi(hardware)
 
         assert not api.is_running
+
+        event_loop = uasyncio.get_event_loop()
+        task = event_loop.create_task(api.start())
+        event_loop.run_forever()
+        await task
+
+        assert api.is_running
