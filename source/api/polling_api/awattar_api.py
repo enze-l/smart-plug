1a376f756124bc55d8ab117cc1723edaf45bb0e9
enze-l <leon.enzenberger@protonmail.com>
2022-12-19 15:16:49 +0100

    Implement api lifecycle

--- source/api/polling_api/awattar_api.py
+++ source/api/polling_api/awattar_api.py
@@ -2,7 +2,10 @@ import urequests
 import time
 import uasyncio
 from micropython import const
+from .awattar_config import TURN_ON_THRESHOLD_EUR
 
+# micropython measure time with seconds since th 1.1.2000
+# to convert this time to utc this variable serves as a reference
 utc_secs_till_2000 = const(946684800)
 
 
@@ -11,21 +14,42 @@ class AwattarApi:
         self.relay = hardware.relay
         self.url = "https://api.awattar.de/v1/marketdata"
         self.is_running = False
+        self.tasks = []
+        self.price_threshold_eur = TURN_ON_THRESHOLD_EUR
 
     def start(self):
         self.is_running = True
         while self.is_running:
-            self.__get_api()
-            await uasyncio.sleep(3)
+            self.__cancel_all_tasks()
+            self.__poll_api()
+            twelve_hours_in_seconds = 12 * 60 * 60
+            await uasyncio.sleep(twelve_hours_in_seconds)
 
-    def __get_api(self):
+    def __poll_api(self):
         res = urequests.get(self.url)
         data = res.json()["data"]
-        print(time.time() + utc_secs_till_2000)
         for interval in data:
-            start_time = int(interval["start_timestamp"])
-            print("Price: " + str(interval["marketprice"]) + " - " + str(start_time))
+            start_time = int(interval["start_timestamp"] / 1000 - utc_secs_till_2000)
+            self.__create_scheduled_task(start_time, interval["marketprice"])
+
+    def __create_scheduled_task(self, start_timestamp, price):
+        current_time = time.time()
+        time_till_execution = start_timestamp - current_time
+        print(time_till_execution)
+        task = uasyncio.create_task(self.__react_to_price_change(price, time_till_execution))
+        self.tasks.append(task)
+
+    def __react_to_price_change(self, price, time_till_execution):
+        await uasyncio.sleep(time_till_execution)
+        print("Executed time: " + str(time_till_execution))
+        if price < self.price_threshold_eur:
+            self.relay.turn_on()
 
     def stop(self):
+        print("tasks canceled")
         self.is_running = False
-        pass
+        self.__cancel_all_tasks()
+
+    def __cancel_all_tasks(self):
+        for task in self.tasks:
+            task.cancel()
