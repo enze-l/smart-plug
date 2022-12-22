91775213ed83781309095a7720cecb7baa59eaed
enze-l <leon.enzenberger@protonmail.com>
2022-12-22 12:14:46 +0100

    Change some import statements to * to make code more readable

--- source/hardware/hardware.py
+++ source/hardware/hardware.py
@@ -1,17 +1,17 @@
 from .binary_output import BinaryOutput
 from .button import Button
-import config.config as config
+from config.config import *
 
-led = BinaryOutput(config.LED_PIN, config.LED_IS_INVERTED)
-relay = BinaryOutput(config.RELAY_PIN, config.RELAY_IS_INVERTED)
+led = BinaryOutput(LED_PIN, LED_IS_INVERTED)
+relay = BinaryOutput(RELAY_PIN, RELAY_IS_INVERTED)
 
 button_internal = Button(
-    config.BUTTON_INTERNAL_PIN,
-    config.BUTTON_INTERNAL_IS_INVERTED,
-    config.BUTTON_EXTERNAL_DEBOUNCE_DELAY,
+    BUTTON_INTERNAL_PIN,
+    BUTTON_INTERNAL_IS_INVERTED,
+    BUTTON_EXTERNAL_DEBOUNCE_DELAY,
 )
 button_external = Button(
-    config.BUTTON_EXTERNAL_PIN,
-    config.BUTTON_EXTERNAL_IS_INVERTED,
-    config.BUTTON_INTERNAL_DEBOUNCE_DELAY,
+    BUTTON_EXTERNAL_PIN,
+    BUTTON_EXTERNAL_IS_INVERTED,
+    BUTTON_INTERNAL_DEBOUNCE_DELAY,
 )
