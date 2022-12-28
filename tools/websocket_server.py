ae4eafede644ad74f61ff963da1761f17a91ac94
enze-l <leon.enzenberger@protonmail.com>
2022-12-28 10:46:54 +0100

    Write concurrent kwebserver

--- tools/websocket_server.py
+++ tools/websocket_server.py
@@ -1,13 +1,28 @@
+from _thread import start_new_thread
 import time
 import socket
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind(("0.0.0.0", 8080))
-    server_socket.listen()
-    connection, address = server_socket.accept()
-    with connection:
-        print(f"Connected by {address}")
+connections = []
+
+
+def connect():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind(("0.0.0.0", 8080))
         while True:
-            time.sleep(1)
-            connection.sendall(b"Hello\n")
-            print("Hello")
+            server_socket.listen()
+            connection, address = server_socket.accept()
+            connections.append(connection)
+
+
+def send():
+    while True:
+        for connection in connections:
+            print("Connected")
+            while True:
+                time.sleep(1)
+                print("Hello")
+                connection.sendall(b"Hello\n")
+
+
+start_new_thread(connect, ())
+send()
