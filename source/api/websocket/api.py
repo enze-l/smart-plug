ac248ad7848c6d965b0d492a69b1c6b1485c151d
enze-l <leon.enzenberger@protonmail.com>
2022-12-28 13:49:28 +0100

    Make websockets run

--- source/api/websocket/api.py
+++ source/api/websocket/api.py
@@ -1,8 +1,7 @@
 import socket
 import uasyncio
-import machine
 from ..abstract_api import AbstractAPI
-from .api_config import SERVER_IP_ADDRESS
+from .api_config import SERVER_IP_ADDRESS, SERVER_PORT
 
 
 class API(AbstractAPI):
@@ -10,28 +9,38 @@ class API(AbstractAPI):
         self.hardware = hardware
         self.connected = False
         self.socket = socket.socket()
+        self.running = False
 
     def start(self):
-        while True:
-            while not self.connected:
-                try:
-                    self.socket.connect((SERVER_IP_ADDRESS, 8080))
-                    self.connected = True
-                    print("connected")
-                except OSError:
-                    print("trying to connect again is 1 second ...")
-                    await uasyncio.sleep(1)
-                    self.socket.close()
-                    self.socket = socket.socket()
-            while self.connected:
-                data = self.socket.recv(1024)
-                message = str(data, "utf8")
-                if len(message) == 0:
-                    self.connected = False
-                else:
-                    print(message)
+        self.running = True
+        while self.running:
+            await self.__init_connection()
+        self.socket.close()
 
+    async def __init_connection(self):
+        while not self.connected:
+            await self.__connect()
+        while self.connected:
+            self.__handle_message()
 
+    async def __connect(self):
+        try:
+            self.socket.connect((SERVER_IP_ADDRESS, SERVER_PORT))
+            self.connected = True
+            print("connected")
+        except OSError:
+            print("trying to connect again is 1 second ...")
+            await uasyncio.sleep(1)
+            self.socket.close()
+            self.socket = socket.socket()
+
+    def __handle_message(self):
+        data = self.socket.recv(1024)
+        message = str(data, "utf8")
+        if len(message) == 0:
+            self.connected = False
+        else:
+            print(message)
 
     def stop(self):
         pass
